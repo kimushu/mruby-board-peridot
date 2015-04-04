443c4593221ceedda8d7e3bdbe0df32929528f46
kimu_shu <alfvegardrisc@gmail.com>
2015-04-04 17:47:15 +0900

    Remove configration-specific code

--- src/peridot.c
+++ src/peridot.c
@@ -1,44 +1,16 @@
 #include "mruby.h"
 #include "mruby/variable.h"
 
-extern const void *__peridot_start_led_base;
-extern const void *__peridot_digital_io_base;
-extern const int __peridot_digital_io_width;
-
-static mrb_value pio_cls;
-static mrb_value start_led;
-static mrb_value digital_io;
-
-static mrb_value
-piocore_new(mrb_state *mrb, mrb_int base, mrb_int width)
+__attribute__((weak)) void
+peridot_class_init(mrb_state *mrb, struct RClass *cls)
 {
-  if (!mrb_test(pio_cls)) {
-    mrb_value mod;
-    mod = mrb_const_get(mrb, mrb_obj_value(mrb->object_class), mrb_intern_lit(mrb, "Altera"));
-    pio_cls = mrb_const_get(mrb, mod, mrb_intern_lit(mrb, "PIOCore"));
-  }
-
-  return mrb_funcall(mrb, pio_cls, "new", 2, mrb_fixnum_value(base), mrb_fixnum_value(width));
+  /* This is a placeholder for override by board specific code */
 }
 
-static mrb_value
-peridot_start_led(mrb_state *mrb, mrb_value self)
+__attribute__((weak)) void
+peridot_class_final(mrb_state *mrb)
 {
-  if (!mrb_test(start_led)) {
-    start_led = piocore_new(mrb, (mrb_int)__peridot_start_led_base, 1);
-  }
-  return start_led;
-}
-
-static mrb_value
-peridot_digital_io(mrb_state *mrb, mrb_value self)
-{
-  if (!mrb_test(digital_io)) {
-    digital_io = piocore_new(mrb,
-                    (mrb_int)__peridot_digital_io_base,
-                    (mrb_int)__peridot_digital_io_width);
-  }
-  return digital_io;
+  /* This is a placeholder for override by board specific code */
 }
 
 void
@@ -52,15 +24,11 @@ mrb_mruby_board_peridot_gem_init(mrb_state *mrb)
   mrb_define_const(mrb, cls, "Author", mrb_str_new_cstr(mrb, "@s_osafune"));
   mrb_define_const(mrb, cls, "Website", mrb_str_new_cstr(mrb, "https://peridotcraft.com/"));
 
-  mrb_define_class_method(mrb, cls, "start_led" , peridot_start_led , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb, cls, "digital_io", peridot_digital_io, MRB_ARGS_NONE());
-
-  pio_cls = mrb_nil_value();
-  start_led = mrb_nil_value();
-  digital_io = mrb_nil_value();
+  peridot_class_init(mrb, cls);
 }
 
 void
 mrb_mruby_board_peridot_gem_final(mrb_state *mrb)
 {
+  peridot_class_final(mrb);
 }
